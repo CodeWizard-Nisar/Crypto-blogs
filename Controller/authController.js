c8aef0e068cbdca0286dfbd8f1fba7ff70345f5e
Inoxentkhan <m.nisarshinwar29@gmail.com>
2023-09-10 20:14:25 +0500

    error handling done for email and username

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,4 +1,5 @@
 const Joi = require("joi");
+const User = require("../Models/users");
 
 const passwordPattern =
   /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!])(?!.*\s).{8,16}$/;
@@ -16,9 +17,33 @@ const authController = {
 
     const { error } = userRegisterSchema.validate(req.body);
 
-    //if error in validation return error via middleware
+    //2. if error in validation return error via middleware
 
     if (error) {
+      return next(error);
+    }
+    //3. if email or username is already registered return an error
+    const { username, name, email, password } = req.body;
+    //check if email is not already  register
+
+    try {
+      const emailInUse = await User.exists({ email });
+      const usernameInUse = await User.exists({ username });
+      if (emailInUse) {
+        const error = {
+          status: 409,
+          message: "Email adress already registered, use another email adress",
+        };
+        return next(error);
+      }
+      if (usernameInUse) {
+        const error = {
+          status: 409,
+          message: "Username not available, choose another different username!",
+        };
+        return next(error);
+      }
+    } catch (error) {
       return next(error);
     }
   },
