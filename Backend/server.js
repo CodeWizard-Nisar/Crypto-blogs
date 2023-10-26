3c7b0d908919a25d1cd84727d173c68a42092bec
Inoxentkhan <m.nisarshinwar29@gmail.com>
2023-10-26 13:22:52 +0500

    Login integration

--- Backend/server.js
+++ Backend/server.js
@@ -4,11 +4,17 @@ const { PORT } = require("./config/index");
 const router = require("./Routes/index");
 const errorHandler = require("./middleware/errorhandler");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const corsOptions = {
+  credentials: true,
+  origin: ["http://localhost:3000"],
+};
 
 const app = express();
 
 app.use(cookieParser());
-
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(router);
--- Front end/coin-bounce/src/Pages/Login/Login.jsx
+++ Front end/coin-bounce/src/Pages/Login/Login.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
 import styles from "./Login.module.css";
 import TextInput from "../../Components/TextInput/TextInput";
 import loginSchema from "../../Schemas/LoginSchema";
 import { useFormik } from "formik";
+import { login } from "../../api/internal";
+import { setUser } from "../../store/userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [error, setError] = useState("");
+
+  const handleLogin = async () => {
+    console.log("login button clicked");
+    const data = {
+      username: values.username,
+      password: values.password,
+    };
+    try {
+      const response = await login(data);
+      if (response.status === 200) {
+        //1. Set user
+        const user = {
+          _id: response.data.user._id,
+          email: response.data.user.email,
+          username: response.data.user.username,
+          auth: response.data.auth,
+        };
+        dispatch(setUser(user));
+
+        //2. Redirect -> homepage
+        navigate("/");
+      } else if (response.code === "ERR_BAD_REQUEST") {
+        // Display error message
+        setError(response.response.data.errormessage);
+      }
+    } catch (error) {
+      // Handle network or server errors here
+      console.error("Login failed:", error);
+    }
+  };
+
   const { values, touched, handleBlur, handleChange, errors } = useFormik({
     initialValues: {
       username: "",
@@ -11,6 +51,7 @@ function Login() {
     },
     validationSchema: loginSchema,
   });
+
   return (
     <div className={styles.loginWrapper}>
       <div className={styles.loginHeader}>Log in to your account</div>
@@ -27,18 +68,26 @@ function Login() {
       <TextInput
         type="password"
         name="password"
-        values={values.password}
+        value={values.password}
         onBlur={handleBlur}
         onChange={handleChange}
         placeholder="password"
         error={errors.password && touched.password ? 1 : undefined}
         errormessage={errors.password}
       />
-      <button className={styles.logInButton}>Log In</button>
+      <button className={styles.logInButton} onClick={handleLogin}>
+        Log In
+      </button>
       <span>
         Don't have an account?{""}
-        <button className={styles.createAccount}>Register</button>
+        <button
+          className={styles.createAccount}
+          onClick={() => navigate("/signup")}
+        >
+          Register
+        </button>
       </span>
+      {error != "" ? <p className={styles.errorMessage}>{error}</p> : ""}
     </div>
   );
 }
